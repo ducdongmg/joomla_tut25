dc5d535f65dcc0c9ec37e8991a1aed11c81ee75b
ducdongmg <ducdong.mg@gmail.com>
2014-02-20 14:23:11 +0700

    language_captcha_akeebaBackup
    cài đặt thêm các thành phần sau
    _language
    _captcha
    _akeebaBackup

---
+++ administrator/components/com_akeeba/akeeba/serverkey.php
@@ -0,0 +1,1 @@
+<?php defined('AKEEBAENGINE') or die(); define('AKEEBA_SERVERKEY', 'NDg0MDI5ZmYzMmEwNzdjNWU1NDc3NDNlMGNmNDExZDY='); ?>
--- configuration.php
+++ configuration.php
@@ -20,8 +20,7 @@ class JConfig {
 	public $live_site = '';
 	public $secret = 'Q9v8zld35BGdnjZl';
 	public $gzip = '0';
-	//public $error_reporting = 'default';
-	public $error_reporting = '6143';
+	public $error_reporting = 'default';
 	public $helpurl = 'http://help.joomla.org/proxy/index.php?option=com_help&keyref=Help{major}{minor}:{keyref}';
 	public $ftp_host = '127.0.0.1';
 	public $ftp_port = '21';
